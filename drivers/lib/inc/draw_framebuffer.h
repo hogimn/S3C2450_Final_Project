323521f9633f38a3f3f4446ceac9a06e6558a1f6
hogimn <hogimn@gmail.com>
2020-12-08 11:33:21 +0900

    draw_framebuffer.h: missing function header added

--- drivers/lib/inc/draw_framebuffer.h
+++ drivers/lib/inc/draw_framebuffer.h
@@ -5,5 +5,6 @@ void init_framebuffer();
 void draw_framebuffer(unsigned char* src, int width, int height);
 void free_framebuffer();
 void clear_framebuffer();
+unsigned short makepixel(unsigned char r, unsigned char g, unsigned char b);
 
 #endif
