c52af4e0f1f74461beba729f5700d42e9da5313c
hogimn <hogimn@gmail.com>
2020-12-08 17:33:00 +0900

    integrate every driver header files to devices.h

---
+++ drivers/lib/inc/devices.h
@@ -0,0 +1,16 @@
+#ifndef __DRIVERS_H__
+#define __DRIVERS_H__
+
+#include "fan.h"
+#include "led.h"
+#include "relay.h"
+#include "magnetic.h"
+#include "moisture.h"
+#include "photo.h"
+#include "servo.h"
+#include "solenoid.h"
+#include "temphumid.h"
+#include "video_capture.h"
+#include "draw_framebuffer.h"
+
+#endif
