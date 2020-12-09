b3b2080902c6bca81ff6c06f8f05fcfce9a59894
rlaqjaals01 <rlaqjaals01@naver.com>
2020-12-09 12:23:29 +0900

    TEMPHUMID_INIT_OK added

--- drivers/lib/inc/temphumid.h
+++ drivers/lib/inc/temphumid.h
@@ -2,9 +2,11 @@
 #define __TEMPHUMID_H__
 
 #define TEMPHUMID_DEV_PATH  "/dev/mds2450_dht11"
-#define TEMPHUMID_OK 1
 
-void temphumid_init(void);
+#define TEMPHUMID_INIT_OK	1
+#define TEMPHUMID_OK	1
+
+int temphumid_init(void);
 int temphumid_read(int ret[2]);
 void temphumid_deinit(void);
 
