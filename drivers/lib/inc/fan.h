d3c827b4ad7d398cec1ab76b9ee9b0a89aa81d72
rlaqjaals01 <rlaqjaals01@naver.com>
2020-12-09 12:22:19 +0900

    addressed erorr

--- drivers/lib/inc/fan.h
+++ drivers/lib/inc/fan.h
@@ -4,11 +4,14 @@
 #include "relay.h"
 #include "mds2450_leadcool120.h"
 
-#define FAN_SPEED_NORMAL    1
-#define FAN_SPEED_FAST      2
-#define FAN_SPEED_VERY_FAST 3
+#define FAN_SPEED_NORMAL    0
+#define FAN_SPEED_FAST      6
+#define FAN_SPEED_VERY_FAST 10
 
-void fan_init(void);
+#define FAN_INIT_OK			1
+#define	FAN_ROTATE_OK 		1
+
+int fan_init(void);
 int fan_rotate(int speed);
 void fan_deinit(void);
 
--- drivers/lib/inc/photo.h
+++ drivers/lib/inc/photo.h
@@ -1,8 +1,12 @@
 #ifndef __PHOTO_H__
 #define __PHOTO_H__
 
-void photo_init(void);
+int photo_init(void);
 int photo_get_intensity(void);
 void photo_deinit(void);
+
+#define PHOTO_INIT_OK              1
+#define PHOTO_INIT_FILE_OPEN_FAIL -1
+#define PHOTO_INIT_I2C_FAIL       -2
 
 #endif
