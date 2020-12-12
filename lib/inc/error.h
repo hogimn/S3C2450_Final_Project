707102b4d231c17f97265c927178be193a135218
hogimn <hogimn@gmail.com>
2020-12-12 22:55:32 +0900

    lib/inc/error.h: ERR_HANDLE macro update

--- lib/inc/error.h
+++ lib/inc/error.h
@@ -1,7 +1,7 @@
 #ifndef __ERROR_H__
 #define __ERROR_H__
 
-#define ERR_HANDLE fprintf(stderr, "%s[%d]: failed\n", \
-        __FUNCTION__, __LINE__); exit(1);
+#define ERR_HANDLE fprintf(stderr, "%s[%d]: %s() failed\n", \
+        __FILE__, __LINE__, __FUNCTION__); exit(1);
 
 #endif
